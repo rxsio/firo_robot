0bbe8bda74fd906b02b439b7c5296c4ad425c1f5
Gabriel Brzezinski <gabrysbrzezinski@gmail.com>
2025-02-02 12:42:55 +0000

    fix: use can_msgs instead of ros2_socketcan_msgs

--- firo_battery_status/firo_battery_status/status_broadcaster.py
+++ firo_battery_status/firo_battery_status/status_broadcaster.py
@@ -1,7 +1,7 @@
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
-from ros2_socketcan_msgs.msg import FdFrame
+from can_msgs.msg import Frame
 from sensor_msgs.msg import BatteryState
 
 
@@ -10,9 +10,9 @@ class StatusBroadcaster(Node):
     def __init__(self):
         super().__init__('status_broadcaster')
         self.publisher_battery_state = self.create_publisher(BatteryState, 'battery_state', 10)
-        self.publisher_can_frame = self.create_publisher(FdFrame, 'can_frame', 10)
+        self.publisher_can_frame = self.create_publisher(Frame, 'can_frame', 10)
         self.get_voltage= self.create_subscription(
-            FdFrame,
+            Frame,
             'can_frame',
             self.battery_callback,
             10)
@@ -21,7 +21,7 @@ class StatusBroadcaster(Node):
         self.timer = self.create_timer(self.get_parameter('update_period').value, self.publish_rtr)
 
     def battery_callback(self, msg):
-        if msg.id != 0x461:
+        if msg.id != 0x461 or msg.is_rtr or msg.dlc != 2:
             return
         data = msg.data
         voltage = float((data[1] << 8) | data[0])
@@ -31,7 +31,9 @@ class StatusBroadcaster(Node):
 
         
     def publish_rtr(self):
-        msg = FdFrame()
+        msg = Frame()
+        msg.header.stamp = self.get_clock().now().to_msg()
+        msg.is_rtr = True
         msg.id = 0x461
         self.publisher_can_frame.publish(msg)
 
