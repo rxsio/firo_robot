5746166ac5df301b617e035925e62c08f4ffb293
KlaNiedz <klaudiacoding@gmail.com>
2025-02-07 00:21:27 +0000

    feat: add converting voltage to percentage

--- firo_battery_status/firo_battery_status/status_broadcaster.py
+++ firo_battery_status/firo_battery_status/status_broadcaster.py
@@ -22,6 +22,10 @@ class StatusBroadcaster(Node):
         self.can_id = self.get_parameter('can_id').value
         self.declare_parameter('update_period', 1.0)
         self.timer = self.create_timer(self.get_parameter('update_period').value, self.publish_rtr)
+        self.declare_parameter('max_voltage', 40.5)
+        self.declare_parameter('min_voltage', 32.0)
+        self.max_voltage = self.get_parameter('max_voltage').get_parameter_value().double_value
+        self.min_voltage = self.get_parameter('min_voltage').get_parameter_value().double_value
 
     def battery_callback(self, msg):
         if msg.id != self.can_id or msg.is_rtr or msg.dlc != 2:
@@ -31,8 +35,16 @@ class StatusBroadcaster(Node):
         # we divide by 1000 to convert from mV to V
         voltage = float(struct.unpack('<H', data)[0]) / 1000.0
         battery = BatteryState()
-        battery.voltage = voltage
+        battery.voltage = voltage        
+        battery.percentage = self.convert_voltage_to_percentage(voltage)
         self.publisher_battery_state.publish(battery)
+
+    def convert_voltage_to_percentage(self, voltage):
+        if voltage < self.min_voltage:
+            return 0.0
+        if voltage > self.max_voltage:
+            return 100.0
+        return (voltage - self.min_voltage) / (self.max_voltage - self.min_voltage) * 100.0
 
         
     def publish_rtr(self):
