a2e770c5a81adfaa93813174c5f9919a1b6fe616
Kacper Marchlewicz <115720673+YoshidoSan@users.noreply.github.com>
2025-09-24 12:52:53 +0200

    Commented discovery server from launch

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,30 +2,27 @@
 set -e
 
 # Set env vars
-export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
-export FASTRTPS_DEFAULT_PROFILES_FILE=/ros2/super_client.xml
+#export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
+#export FASTRTPS_DEFAULT_PROFILES_FILE=/ros2/super_client.xml
 
 # Source ROS setup
 source /ros2/install/setup.bash
 
 # Reset ros daemon
-ros2 daemon stop
-ros2 daemon start
-
+#ros2 daemon stop
+#ros2 daemon start
 # Give 2 seconds for daemon to setup
-sleep 2
-
+#sleep 2
 # Start discovery server in the background
-fastdds discovery -i 0 -l 100.75.18.108 -p 11811 &
-DISCOVERY_PID=$!
-
+#fastdds discovery -i 0 -l 100.75.18.108 -p 11811 &
+#DISCOVERY_PID=$!
 # Give 2 seconds for server to setup
-sleep 2
+#sleep 2
 
 # Start ROS launch
 ros2 launch firo_bringup firo_bringup.launch &
 LAUNCH_PID=$!
 
 # Wait for both to keep container alive
-wait $DISCOVERY_PID
+#wait $DISCOVERY_PID
 wait $LAUNCH_PID
